8e68b01be1f4426f6f3fa3e9e4eac6044361aed2
Minh Nguyen <thienphong215@gmail.com>
2017-04-02 03:51:48 +0700

    Scraping posts

---
+++ posts.js
@@ -0,0 +1,46 @@
+const scrapeIt = require("scrape-it");
+
+var id = 5948445;
+
+scrapeIt(`https://vozforums.com/showthread.php?t=${id}`, {
+    // Fetch the articles
+    posts: {
+        listItem: "#posts > div"
+        , data: {
+
+            postCount: {
+                selector: "tr:first-child td div:first-child a:first-child",
+                attr: "name",
+                convert: text => parseInt(text)
+            }
+
+            , postId: {
+                selector: "tr:first-child td div:first-child a:first-child",
+                attr: "href",
+                convert: url => parseInt(url.split("=")[1].split("&")[0])
+            }
+
+            , time: {
+                selector: "tr:first-child td.thead div:nth-child(2)"
+            }
+
+            , userID: {
+                selector: ".bigusername",
+                attr: "href",
+                convert: text => text.split("u=")[1]
+            }
+
+            , userName: {
+                selector: ".bigusername"
+            }
+
+            , content: {
+                selector: ".voz-post-message"
+            }
+
+        }
+    }
+
+}, (err, page) => {
+    console.log(err || page);
+});
