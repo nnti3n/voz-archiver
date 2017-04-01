d0a96580a29d804ad2e12d4e1df8a8f03d478938
Minh Nguyen <thienphong215@gmail.com>
2017-04-02 03:52:08 +0700

    Database schema

---
+++ schema/database.js
@@ -0,0 +1,12 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://nntien:dragonquest@localhost:5432/voz_development';
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query_threads = client.query(
+  'CREATE TABLE threads(id INTEGER PRIMARY KEY, title TEXT not null, source TEXT, pages INT not null, last_updated TIMESTAMPTZ not null)');
+query_threads.on('end', () => { client.end(); });
+
+const query_posts = client.query(
+  'CREATE TABLE posts(id INTEGER PRIMARY KEY, post_count INT not null, user_id VARCHAR(40) not null, user_name VARCHAR(40) not null, content TEXT, time VARCHAR(40) not null, thread_id INTEGER REFERENCES threads not null)');
+query_posts.on('end', () => { client.end(); });
